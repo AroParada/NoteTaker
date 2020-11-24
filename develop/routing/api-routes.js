09e6b7ff83a401a75c62ef869f0d8f7b60bd5771
harold parada <haroldparada20@gmail.com>
2020-11-24 01:49:40 -0500

    app.post added

--- develop/routing/api-routes.js
+++ develop/routing/api-routes.js
@@ -8,17 +8,25 @@ module.exports = function(app) {
   app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../db/db.json"));
   });
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
+ 
    app.post("/api/notes", function(req, res) {
         var newNote = req.body;
+        console.log(req.body)
+        fs.readFile("./db/db.json", (err, data) => {
+			    if (err) throw err;
+			    const allTheNotes = JSON.parse(data);
+			    allTheNotes.push(newNote);
+            fs.writeFile(
+              "./db/db.json",
+              JSON.stringify(allTheNotes, null, 2),
+              (err) => {
+                if (err) throw err;
+                res.json(noteData);
+                console.log("note has been created");
+              }
+            );
+          });
   });
-
   app.delete("/api/notes", function(req, res) {
 
   });
